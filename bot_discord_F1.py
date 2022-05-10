aeed34e0c8865cbca1fa5f2ed7fa111a1ef49ceb
Lucas <lucasfrancs.contato@gmail.com>
2022-05-10 00:17:12 -0300

    Done it!

--- bot_discord_F1.py
+++ bot_discord_F1.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-#from decouple import config
+from decouple import config
 import grandprix as gp
 import consultDatabase as cdb
 
@@ -9,7 +9,6 @@ mainRoomID = 586582112388382731
 
 robot = commands.Bot("!")
 
-client = discord.Client()
 
 #Este é um evento que indica que o bot está pronto para ser executado
 @robot.event 
@@ -22,58 +21,6 @@ async def on_ready():
 
 
 
-@robot.command(name="comandos")
-async def allCommands(ctx):
-    username = ctx.author.name
-    channel = robot.get_channel(mainRoomID)
-
-    with open(gp.pathCommands+"allCommands.txt",encoding="utf-8") as x:
-        allCommands = x.read()
-
-    comandos = f"Olá, {username}. Estes são os comandos:\n"+allCommands
-
-    await channel.send(comandos)
-
-
-
-@robot.command(name="listagp")
-async def consultaGPs(ctx):
-    username = ctx.author.name
-    channel = robot.get_channel(mainRoomID)
-    allcommandsGP = gp.allcommandsGP
-
-
-    response = f"{username}, para consultar informações dos grandes prêmios, digite:\n"+str(allcommandsGP)
-
-
-    await channel.send(response)
-
-
-
-@robot.command(name="proximogp")
-async def grandPrix(ctx):
-    username = ctx.author.name
-    channel = robot.get_channel(mainRoomID)
-
-    #channel = robot.get_channel(mainRoomID)
-
-    response = "O próximo Grande Prêmio será:\n***Grande Prêmio de Miami***\n**Data:** 08/05/2022\n**Horário (Brasília):** 16h30\n**Pole position:** N/A"
-    try:
-        await channel.send(response)
-    except Exception as e:
-        print(e)
-
-
-@robot.command(name="link")
-async def link(ctx):
-    link = r"https://www.band.uol.com.br/esportes/automobilismo/formula-1/ao-vivo"
-    response = "Band Fórmula 1 - Link: "+link
-
-    channel = robot.get_channel(mainRoomID)
-
-    await channel.send(response)
-
-
 #@robot.command(name="gpBahrein")
 @robot.event
 async def on_message(msg):
@@ -82,25 +29,61 @@ async def on_message(msg):
     if msg.author == robot.user:
         return
 
-    if msg.content.notstartswith("!"):
+    if msg.content.startswith("!"):
+        listaGP = ["gpBahrein", "gpArábiaSaudita", "gpAustrália", "gpEmíliaRomanha", "gpMiami", "gpEspanha", "gpMônaco", "gpAzerbaijão", "gpCanadá", "gpInglaterra", "gpÁustria", "gpFrança", "gpHungria", "gpBélgica", "gpHolanda", "gpItália", "gpSingapura", "gpJapão", "gpEUA", "gpMéxico", "gpSãoPaulo", "gpAbuDhabi"]
+        
+        msgcontent = msg.content
+
+        if msg.content[1:] in listaGP:
+            msgg = msg.content
+            gpName = cdb.consultGP.consultGPname(msgg[1:])
+            gpDate = cdb.consultGP.consultGPdate(msgg[1:])
+            gpTime = cdb.consultGP.consultGPtime(msgg[1:])
+            channel = robot.get_channel(586582112388382731)
+            link = r"https://www.band.uol.com.br/esportes/automobilismo/formula-1/ao-vivo"
+
+            response = f"***{gpName[5:]}***\n**Data:** {gpDate[5:]}\n**Horário (Brasília):** {gpTime[5:]}\n**Link: **{link}"
+
+            await channel.send(response)
+
+
+
+        elif msg.content[1:] == "comandos":
+            username = msg.author.name
+            channel = robot.get_channel(mainRoomID)
+
+            with open(gp.pathCommands+"allCommands.txt",encoding="utf-8") as x:
+                allCommands = x.read()
+
+            comandos = f"Olá, {username}. Estes são os comandos:\n"+allCommands
+
+            await channel.send(comandos)
+
+
+
+        elif msg.content[1:] == "listagp":
+            username = msg.author.name
+            channel = robot.get_channel(mainRoomID)
+            allcommandsGP = gp.allcommandsGP
+
+            response = f"{username}, para consultar informações dos grandes prêmios, digite:\n"+str(allcommandsGP)
+
+            await channel.send(response)
+
+
+
+        elif msg.content[1:] == "link":
+            link = r"https://www.band.uol.com.br/esportes/automobilismo/formula-1/ao-vivo"
+            response = "Band Fórmula 1 - Link: "+link
+
+            channel = robot.get_channel(mainRoomID)
+
+            await channel.send(response)
+
+    else:
         return
 
-    listaGP = ["gpBahrein", "gpArábiaSaudita", "gpAustrália", "gpEmíliaRomanha", "gpMiami", "gpEspanha", "gpMônaco", "gpAzerbaijão", "gpCanadá", "gpInglaterra", "gpÁustria", "gpFrança", "gpHungria", "gpBélgica", "gpHolanda", "gpItália", "gpSingapura", "gpJapão", "gpEUA", "gpMéxico", "gpSãoPaulo", "gpAbuDhabi"]
-    if msg.content in listaGP:
-        msgg = msg.content
-        gpName = cdb.consultGP.consultGPname(msgg)
-        gpDate = cdb.consultGP.consultGPdate(msgg)
-        gpTime = cdb.consultGP.consultGPtime(msgg)
-        channel = robot.get_channel(586582112388382731)
-        link = r"https://www.band.uol.com.br/esportes/automobilismo/formula-1/ao-vivo"
-
-        response = f"***{gpName[5:]}***\n**Data:** {gpDate[5:]} (Ano-Mês-Dia)\n**Horário (Brasília):** {gpTime[5:]}\n**Link: **{link}"
-
-        await channel.send(response)
-    else:
-        await channel.send("mamou")
 
 
-
-#TOKEN_DD = config("PRIVATETOKEN")
-robot.run("OTY0NjAxNTEwMzA4NzQ5Mzg1.GqvhSD.YEndDsMOkVS8EQb5xOsxr_e5iiPUgMjbPuCMyo")
+TOKEN_DD = config("PRIVATETOKEN")
+robot.run(TOKEN_DD)
--- consultDatabase.py
+++ consultDatabase.py
@@ -1,10 +1,8 @@
-
 import pandas as pd
-import datetime
 
-db_path = 'databases\\'
+db_path = 'I:\\Documentos\\Programacao\\Repos\\GitHub\\BotF1discord\\databases\\'
 
-df = pd.read_excel("main_database.xlsx",sheet_name="db_structuredDatas")
+df = pd.read_excel(db_path+"main_database.xlsx",sheet_name="db_structuredDatas")
 
 
 #Classe de consulta das informações das corridas
@@ -13,17 +11,17 @@ class consultGP:
     #Função para retornar o nome do Grande Prêmio
     def consultGPname(cod):
         cod = str(cod)
-        n = df.loc[df['command'] == cod, 'gpName',].to_string()
+        n = df.loc[df['command'] == cod, 'gpName'].to_string()
         return n
 
     #Função para retornar a data do Grande Prêmio
     def consultGPdate(cod):
         cod = str(cod)
-        n = df.loc[df['command'] == cod, 'date_race',].to_string()
+        n = df.loc[df['command'] == cod, 'date_race'].to_string()
         return n
 
     #Função para retornar a hora do Grande Prêmio
     def consultGPtime(cod):
         cod = str(cod)
-        n = df.loc[df['command'] == cod, 'startTime_race',].to_string()
+        n = df.loc[df['command'] == cod, 'startTime_race'].to_string()
         return n
